52f38806a97c548867600217dd0e2580ebc6fd16
Mohammed Al-Thawr <malthawr@gmail.com>
2020-09-22 11:05:03 +0000

    Automatically backed up by Learn

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,32 @@ class App extends React.Component {
     }
   }
 
+  onChangeType = (event) => {
+      this.setState(
+        {
+          filters: Object.assign({} ,this.state.filters , {type : event.target.value})
+        }
+      )
+    }
+  
+    onFindPets = () => {
+      let url = "/api/pets" ;
+  
+      if (this.state.filters.type !=="all") {
+        url += `?type=${this.state.filters.type}`;
+      }
+         fetch(url).then(Response => Response.json())
+                   .then(json => this.setState({pets : json})) ;
+    }
+    adoptPet = (id)=> {
+        let pets= this.state.pets.map(pet => {
+          if(id === pet.id ) {
+            return Object.assign(pet,{isAdopted: true })
+          }
+       });
+        this.setState({ pets: pets });
+    }
+
   render() {
     return (
       <div className="ui container">
